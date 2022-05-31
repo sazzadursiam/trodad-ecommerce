965fe77eb59a6f7a943d478be15c92e11a35abed
Shazed94 <ashraf.shazed@gmail.com>
2022-05-31 14:01:10 +0600

    React app Install for ecommerce

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import AdminDashboard from "./Pages/Admin/AdminDashboard";
+import UserDashboard from "./Pages/User/UserDashboard";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/user" element={<UserDashboard />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Pages/Admin/AdminDashboard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AdminDashboard = () => {
+  return (
+    <div>Hi I'm Admin Dashboard</div>
+  )
+}
+
+export default AdminDashboard
